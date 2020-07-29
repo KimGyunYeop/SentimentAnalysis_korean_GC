c5d8f6a072cfd576ac25e20bed654e5a90dfff26
hyeonjichoi <ckatodmlrna@naver.com>
2020-07-29 17:13:04 +0900

    fix txt return

--- test.py
+++ test.py
@@ -154,14 +154,11 @@ def main(cli_args):
 
         pred_and_labels = pd.DataFrame([])
         pred_and_labels["data"] = txt_all
-        print(len(preds))
-        print(len(labels))
-        print(len(preds==labels))
         pred_and_labels["pred"] = preds
         pred_and_labels["label"] = labels
         pred_and_labels["result"] = preds==labels
 
-        pred_and_labels.to_csv(os.path.join(checkpoint,"test_result.csv"), encoding = "utf-8")
+        pred_and_labels.to_csv(os.path.join(checkpoint,"test_result.csv"),seq="\t", encoding = "utf-8")
 
             
 
