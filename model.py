49ce581d5d5dc02a4b35584884c596bcd92472c2
KimGyunYeop <gyop0817@naver.com>
2020-07-29 15:48:37 +0900

    dot product fix

--- model.py
+++ model.py
@@ -195,7 +195,10 @@ class LSTM_ATT_DOT(nn.Module):
         self.att_w = nn.Parameter(torch.randn(1, 768, 1))
 
     def attention_net(self, lstm_output, final_state):
+        print(lstm_output.shape)
+        print(final_state.permute(1, 2, 0).shape)
         attn_weights = torch.bmm(lstm_output, final_state.permute(1, 2, 0))
+        print(attn_weights.shape)
         soft_attn_weights = F.softmax(attn_weights, 1).unsqueeze(2)  # shape = (batch_size, seq_len, 1)
         print(lstm_output.shape)
         print(soft_attn_weights.shape)
