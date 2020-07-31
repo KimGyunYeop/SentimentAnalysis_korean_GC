5287435c662e29323053bd4f3f316f0b648a771c
hyeonjichoi <ckatodmlrna@naver.com>
2020-07-31 12:21:38 +0900

    Add kosac version

--- datasets.py
+++ datasets.py
@@ -70,7 +70,7 @@ class KOSACDataset(Dataset):
         intensities = []
 
         for i in range(len(dataset)):
-            tokens = self.tokenizer._tokenize(dataset.at[i,'review'])
+            tokens = self.tokenizer._tokenize(dataset.at[int(i),'review'])
             polarity = []
             intensity = []
             polarity.append('None')
