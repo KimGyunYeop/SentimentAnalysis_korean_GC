ecc3bf9227c6fd4449db83a5e092303f7838dad4
HyeonJiChoi <44078732+HyeonJiChoi@users.noreply.github.com>
2020-08-07 14:01:10 +0900

    Add att loss

--- model.py
+++ model.py
@@ -491,12 +491,12 @@ class KOSAC_LSTM_ATT_DOT_ML(nn.Module):
         outputs = self.dense(attn_output)
         outputs = self.dropout(outputs)
         outputs = self.out_proj(outputs)
-        print(soft_attn_weights.squeeze())
+        print(soft_attn_weights)
         att_label = F.softmax(torch.abs(polarity_ids)+torch.abs(intensity_ids))
         print(att_label.size())
         loss_fct = nn.CrossEntropyLoss()
         loss_att = nn.CrossEntropyLoss()
-        loss = (loss_fct(outputs.view(-1, 2), labels.view(-1)) + loss_att(soft_attn_weights,att_label))/2
+        loss = (loss_fct(outputs.view(-1, 2), labels.view(-1)) + loss_att(soft_attn_weights.squeeze(),att_label))/2
 
         result = (loss, outputs)
         return result
