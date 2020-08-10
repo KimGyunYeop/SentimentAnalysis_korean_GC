9e3e6b510d71352782537a495056d74088fbb7a4
KimGyunYeop <gyop0817@naver.com>
2020-08-10 15:31:21 +0900

    fix train.py for make small

--- small.sh
+++ small.sh
@@ -1,1 +1,1 @@
-python3 train_small.py --task nsmc --config_file koelectra-base.json --result_dir koeletra-lstm-att-dot-cls --model_mode LSTM_ATT_DOT --gpu 0 --small True
+python3 train.py --task nsmc --config_file koelectra-base.json --result_dir koeletra-lstm-att-dot-cls --model_mode LSTM_ATT_DOT --gpu 0 --small True
