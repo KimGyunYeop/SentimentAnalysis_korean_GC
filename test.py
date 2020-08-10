340c498da92bac1ca82576c70aae1e11484cb1da
KimGyunYeop <gyop0817@naver.com>
2020-08-10 16:13:05 +0900

    fix test

--- test.py
+++ test.py
@@ -163,7 +163,7 @@ def main(cli_args):
     logger.info("Testing model checkpoint to {}".format(max_checkpoint))
     global_step = max_checkpoint.split("-")[-1]
     model = MODEL_LIST[cli_args.model_mode](args.model_type, args.model_name_or_path, config)
-    model.load_state_dict(torch.load(max_checkpoint+"/training_model.bin"))
+    model.load_state_dict(torch.load(os.path.join("ckpt",cli_args.result_dir,max_checkpoint,"training_model.bin")))
     model.to(args.device)
     preds, labels, result, txt_all = evaluate(args, model, test_dataset, mode="test", global_step=global_step)
 
