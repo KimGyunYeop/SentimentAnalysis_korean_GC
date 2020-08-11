3c359fcad4b13ff50089a85b9be056c7ee7a6947
HyeonJiChoi <44078732+HyeonJiChoi@users.noreply.github.com>
2020-08-12 00:58:28 +0900

    Add GTR loss

--- model.py
+++ model.py
@@ -408,7 +408,7 @@ class KOSAC_LSTM_ATT_DOT(nn.Module):
         # Embedding
         self.input_embedding = self.emb.embeddings.word_embeddings
         self.polarity_embedding = nn.Embedding(5, 768)
-        self.intensity_embedding = nn.Embedding(5, 768)
+        self.intensity_embedding = nn.Embedding(4, 768)
 
         self.lstm = nn.LSTM(768, 768, batch_first=True, bidirectional=False)
         self.lstm_dropout = nn.Dropout(0.2)
@@ -456,7 +456,7 @@ class KOSAC_LSTM_GTR(nn.Module):
 
         # Embedding
         self.polarity_embedding = nn.Embedding(5, 768)
-        self.intensity_embedding = nn.Embedding(5, 768)
+        self.intensity_embedding = nn.Embedding(4, 768)
         self.lstm = nn.LSTM(768, 768, batch_first=True, bidirectional=False)
         self.lstm_dropout = nn.Dropout(0.2)
         self.dense = nn.Linear(768, 1)
