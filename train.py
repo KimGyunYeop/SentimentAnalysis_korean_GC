4377c79c572614f237b33f8f46fa35d30eabb094
HyeonJiChoi <44078732+HyeonJiChoi@users.noreply.github.com>
2020-08-19 22:00:50 +0900

    Add GTR loss

--- train.py
+++ train.py
@@ -268,6 +268,7 @@ def main(cli_args):
     args.device = "cuda:{}".format(cli_args.gpu) if torch.cuda.is_available() and not args.no_cuda else "cpu"
     print("a")
     model.to(args.device)
+    print("a")
     if cli_args.small == False:
         # Load dataset
         train_dataset = DATASET_LIST[cli_args.model_mode](args, tokenizer, mode="train") if args.train_file else None
