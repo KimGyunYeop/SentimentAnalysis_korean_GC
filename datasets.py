a7f41f9690f3365375cee7f8edc3f9fd35c79956
hyeonjichoi <ckatodmlrna@naver.com>
2020-08-19 17:04:15 +0900

    Add lexicon tokenizer

--- datasets.py
+++ datasets.py
@@ -216,10 +216,11 @@ class KNUDataset(Dataset):
             for key in sorted_key:
                 one_polarity_list = self.find_sub_list(list(key),tokens)
                 for start,end in one_polarity_list:
-                    if(polarity>48):
+                    if(len(polarity)>48):
                         print(key)
                         print(one_polarity_list)
                         print(polarity)
+                        print(len(polarity))
                     polarity[start:end+1] = [key2pol[key]]*(end-start+1)
 
             polarity=[0]+polarity+[0]
