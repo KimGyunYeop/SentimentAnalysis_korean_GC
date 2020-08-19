9668cb74a7034cd0db714a48f6729870a06ac6ac
hyeonjichoi <ckatodmlrna@naver.com>
2020-08-19 17:48:25 +0900

    Add lexicon tokenizer

--- model.py
+++ model.py
@@ -827,9 +827,9 @@ class KNU_LSTM_ATT_DOT(nn.Module):
         # embedding
         input_emb_result = self.input_embedding(input_ids)
         polarity_emb_result = self.polarity_embedding(polarity_ids)
+        print(attention_mask[0])
 
         embedding_result = input_emb_result + polarity_emb_result / 100
-        print(embedding_result.shape)
         outputs = self.emb(input_ids=None, attention_mask=attention_mask, token_type_ids=token_type_ids,inputs_embeds = embedding_result)
         outputs, (h, c) = self.lstm(outputs[0])
         attn_output = self.attention_net(outputs, h)
