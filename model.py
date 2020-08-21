89e24384bf589962689cbb5cfcb5bc25b40b7a14
HyeonJiChoi <44078732+HyeonJiChoi@users.noreply.github.com>
2020-08-21 15:21:09 +0900

    Add Mix att

--- model.py
+++ model.py
@@ -460,9 +460,8 @@ class LSTM_ATT_MIX(nn.Module):
         att_outputs = []
         batch_size, seq_len, w2v_dim = lstm_outputs.shape
         lstm_outputs = torch.nn.functional.pad(lstm_outputs, (0, 0, 1, 1))
-        print(len(self.word_base_att))
         for i in range(1, 51):
-            lstm_output, a = self.word_base_att[i](lstm_outputs[:,i-1:i+2,:].squeeze())
+            lstm_output, a = self.word_base_att[i - 1](lstm_outputs[:,i-1:i+2,:].squeeze())
             att_outputs.append(self.concat_att(lstm_output,a))
 
         print(len(att_outputs))
