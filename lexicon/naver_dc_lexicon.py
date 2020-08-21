bdc737d02c84312a50f9d1c98fa836b2a43c1e5b
soyoung <44626132+soyoungpark9187@users.noreply.github.com>
2020-08-22 04:21:22 +0900

    Update naver_dc_lexicon.py
    sentiment revised

--- lexicon/naver_dc_lexicon.py
+++ lexicon/naver_dc_lexicon.py
@@ -3,21 +3,20 @@ import pandas as pd
 from selenium import webdriver
 import time
 
-import random
-
 urls = []
-# 현재 단어와 유의어인지 반의어인지
-rev = []
-
+rev = [] # 현재 단어와 유의어인지 반의어인지
 results = pd.DataFrame()
 words = []
 sentiments = []
-adj_sents = []
 tests = []
+
+adj_sents = []
 
 test = 0
 
 searched = []
+
+max_depth = 10
 
 chromeDriver = "C:\\Users\\parksoyoung\\Downloads\\chromedriver_win32\\chromedriver.exe"
 driver = webdriver.Chrome(chromeDriver)
@@ -27,15 +26,10 @@ time.sleep(3)
 # 시작 단어가 꽃답다(긍정)이기 때문에
 senti = 1
 
-# 검색한 단어
-searched.append("꽃답다")
-
 # 초기 단어
 words.append("꽃답다")
 sentiments.append(senti)
 tests.append(test)
-
-li = ['', '']
 
 source = driver.page_source
 soup = BeautifulSoup(source, "html.parser")
@@ -44,6 +38,7 @@ soup = BeautifulSoup(source, "html.parser")
 def oneWord(sent):
     source = driver.page_source
     soup = BeautifulSoup(source, "html.parser")
+
     # 활용 어간
     dl = soup.find_all("dl")
     ct = 0
@@ -54,37 +49,24 @@ def oneWord(sent):
     if ct == 1:
         aps = soup.find("dl", {"class": "entry_conjugation"}).find("dd", {"class": "cont"}).find("ul", {
             "class": "tray"}).find_all("li", {"class": "item"})
-    for ap in aps:
-        word = ap.find("span", {"class": "u_word_dic"}).text.strip()
-        words.append(word)
-        sentiments.append(sent)
-        tests.append(test)
-
-    s = soup.find("input", {"id":"hiddenQuery"})
-    searched.append(s.get("value"))
+        '''    s = soup.find("input", {"id":"hiddenQuery"})
+    searched.append(s.get("value"))'''
+        for ap in aps:
+            word = ap.find("span", {"class": "u_word_dic"}).text.strip()
+            words.append(word)
+            if d.get("class")[0] == 'antonym':
+                sentiments.append(sent * -1)
+            else:
+                sentiments.append(sent)
+            tests.append(test)
 
     # 유의어, 반의어
     dc = soup.find("div", {"class": "slides_content _slides_content _visible"})
     div = dc.find_all("div")
-    r = random.randint(1, 2)
-    # 초기값: 유의어
     for d in div:
-        if d.get("class")[0] == 'synonym':
-            li = d.get("class")
-    for d in div:
-        # 유의어, 반의어 중에 랜덤으로 누르기
-        if r == 1:
-            if d.get("class")[0] == 'synonym':
-                li = d.get("class")
-        elif r == 2:
-            if d.get("class")[0] == 'antonym':
-                li = d.get("class")
-
         if d.get("class")[0] != 'key_word':
+            sa = d.get("class")[0]
             em = d.find_all("em")
-            if d.get("class")[0] == 'antonym':
-                # 반대말 단어들
-                sent *= -1
             ym = d.get("class")
             i=0
             for e in em:
@@ -93,9 +75,11 @@ def oneWord(sent):
                 if word[-1] == '1' or word[-1] == '2' or word[-1] == '3' or word[-1] == '4' or word[-1] == '5':
                     word = word[0:-1]
                 words.append(word)
-                sentiments.append(sent)
+                if sa == 'antonym':
+                    sentiments.append(sent * -1)
+                else:
+                    sentiments.append(sent)
                 tests.append(test)
-                # 유의어/반의어의 활용 어간
                 concat_ym = ym[0] + ' ' + ym[1]
                 i+=1
                 driver.find_element_by_xpath(
@@ -107,14 +91,16 @@ def oneWord(sent):
                     '/a[2]').click()
                 # 가장 최근 연 탭으로
                 driver.switch_to.window(driver.window_handles[-1])
-                # url 저장
-                #print(driver.current_url)
                 urls.append(driver.current_url)
-                if ym[0] == 'synonym':
-                    adj_sents.append(sent)
-                else:
-                    adj_sents.append(sent*-1)
                 time.sleep(3)
+                if sa == 'synonym':
+                    #start(driver.current_url, sent)
+                    ap_sent = sent
+                else:
+                    #start(driver.current_url, sent*-1)
+                    ap_sent = sent*-1
+                adj_sents.append(ap_sent)
+                #print(sent, driver.current_url, ap_sent)
                 source = driver.page_source
                 soup = BeautifulSoup(source, "html.parser")
                 # 활용 어간
@@ -127,42 +113,43 @@ def oneWord(sent):
                 if ct == 1:
                     aps = soup.find("dl", {"class": "entry_conjugation"}).find("dd", {"class": "cont"}).find("ul", {
                         "class": "tray"}).find_all("li", {"class": "item"})
-                for ap in aps:
-                    word = ap.find("span", {"class": "u_word_dic"}).text.strip()
-                    words.append(word)
-                    sentiments.append(sent)
-                    tests.append(test)
+                    for ap in aps:
+                        word = ap.find("span", {"class": "u_word_dic"}).text.strip()
+                        words.append(word)
+                        if sa == 'antonym':
+                            sentiments.append(ap_sent)
+                        else:
+                            sentiments.append(ap_sent)
+                        tests.append(test)
 
                 # 현재 탭 닫고 맨 처음 탭으로 변경 (0번 탭)
                 driver.close()
                 driver.switch_to.window(driver.window_handles[0])
-    return li
+    return 0
 
 time.sleep(3)
 
 def start(link, senti):
+    #driver = webdriver.Chrome(chromeDriver)
     driver.get(link)
     time.sleep(3)
     source = driver.page_source
     soup = BeautifulSoup(source, "html.parser")
     oneWord(senti)
-
-# 꽃답다, 긍정
-#start('https://ko.dict.naver.com/#/entry/koko/1b3ddffea9e44296a09842b0776d6997', 1)
 senti = 1
 urls.append('https://ko.dict.naver.com/#/entry/koko/1b3ddffea9e44296a09842b0776d6997')
 rev.append(1)
 adj_sents.append(1)
 
+idx=0
 while True:
     prev = len(words)
-    urls = list(set(urls))
-    print(len(urls))
-    for i in range(len(urls)):
+    #urls = list(set(urls)) # 중복 제거
+    for i in range(idx, len(urls)):
         results = pd.DataFrame()
-        #print(urls[i])
         start(urls[i], adj_sents[i])
-        if prev == len(words):
+        idx = i
+        if test == max_depth or prev == len(words):
             break
         results['word'] = words
         results['sentiment'] = sentiments
@@ -172,3 +159,4 @@ while True:
         print(len(uni))
         results.to_csv('naver_dc_꽃답다.txt', encoding="utf8", sep="\t")
         uni.to_csv('naver_dc_꽃답다_uni.txt', encoding="utf8", sep="\t")
+    test+=1
