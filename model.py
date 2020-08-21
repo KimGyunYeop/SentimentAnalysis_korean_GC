4709408347479c83e8a284c10ed1d5f403b7db5d
HyeonJiChoi <44078732+HyeonJiChoi@users.noreply.github.com>
2020-08-21 18:16:58 +0900

    Add Mix att

--- model.py
+++ model.py
@@ -470,6 +470,7 @@ class LSTM_ATT_MIX(nn.Module):
         lstm_output, a = self.total_word_att(inputs)
         alpha = 0.8
         for index in range(batch_size):
+            print(50-data_length[0])
             a[index][data_length:] = torch.zeros(50-data_length[0])
             a3[index][data_length:] = torch.zeros(50-data_length[0])
         print(a[0])
