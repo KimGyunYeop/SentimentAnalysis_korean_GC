1c5f621c899aebc9492f5c5bac21bd80e8285613
HyeonJiChoi <44078732+HyeonJiChoi@users.noreply.github.com>
2020-08-21 13:25:16 +0900

    Add Mix att

--- model.py
+++ model.py
@@ -457,7 +457,7 @@ class LSTM_ATT_MIX(nn.Module):
             att_outputs.append(self.word_base_att[i](lstm_outputs[:,i:i+3,:].squeeze()))
 
         inputs = torch.cat(att_outputs,dim=-1)
-        print(inputs.shape)
+        inputs = torch.reshape(inputs, (-1,50,768))
         att_output = self.gram_3_att(inputs)
 
         return att_output
