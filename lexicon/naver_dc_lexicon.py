37ac4ba2e3ecb208e9eb1f5c1051469325bed35a
KimGyunYeop <gyop0817@naver.com>
2020-08-23 20:39:10 +0900

    `improve cos loss with star`

--- lexicon/naver_dc_lexicon.py
+++ lexicon/naver_dc_lexicon.py
@@ -3,17 +3,20 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
 
+all_urls = []
+
 MAX_LEVEL = 5
 
 def search_node(url, sentiment, level):
     #get now word's dict homepage
     driver.get(url)
-    time.sleep(0.3)
+    time.sleep(1)
     bs = BeautifulSoup(driver.page_source, "html.parser")
 
     #get word in metadata
     word_meta = bs.find("meta",{"property":"og:title"})
     cur_word = word_meta.get("content").split("\'")[1]
+    print(cur_word, level)
     global sent_dc
     sent_dc = sent_dc.append(pd.Series([cur_word, sentiment, level], index=sent_dc.columns), ignore_index=True)
 
@@ -50,12 +53,18 @@ def search_node(url, sentiment, level):
         if "synonym" in tmp_box.get("class"):
             synonym_list_box = tmp_box.findAll("em")
             for i in synonym_list_box:
-                synonym_list.append(i.find("a",{"class":"blank"}).get("href"))
+                url = i.find("a",{"class":"blank"}).get("href")
+                if url not in all_urls:
+                    synonym_list.append(url)
+                    all_urls.append(url)
 
         if "antonym" in tmp_box.get("class"):
             antonym_list_box = tmp_box.findAll("em")
             for i in antonym_list_box:
-                antonym_list.append(i.find("a",{"class":"blank"}).get("href"))
+                url = i.find("a",{"class":"blank"}).get("href")
+                if url not in all_urls:
+                    antonym_list.append(url)
+                    all_urls.append(url)
 
     #print(antonym_list,synonym_list)
 
@@ -67,11 +76,12 @@ def search_node(url, sentiment, level):
 
 
 baselink = "https://ko.dict.naver.com/"
-first_word_url = "#/entry/koko/61a8e24d442d4b47a7a241797f968195"
-chromeDriver = "C:\\Users\\parksoyoung\\Downloads\\chromedriver_win32\\chromedriver.exe"
+chromeDriver = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chromedriver.exe"
 driver = webdriver.Chrome(chromeDriver)
-sent_dc = pd.DataFrame(columns=["word",'sentiment',"level"])
 
+'''sent_dc = pd.DataFrame(columns=["word",'sentiment',"level"])
+first_word_url = "#/entry/koko/61a8e24d442d4b47a7a241797f968195"
+all_urls = [first_word_url]
 try:
     search_node(baselink+first_word_url, 1, 0)
 except:
@@ -79,5 +89,198 @@ except:
 
 print(sent_dc)
 sent_dc.to_csv("result_예쁘다.csv", encoding='utf-8-sig')
+all_urls = [first_word_url]
+# 흥미롭다
+sent_dc = pd.DataFrame(columns=["word",'sentiment',"level"])
+first_word_url = "#/entry/koko/b9fbbe6ab1734c93ba2213b4ac22fbba"
 
+try:
+    search_node(baselink+first_word_url, 1, 0)
+except:
+    pass
+
+print(sent_dc)
+sent_dc.to_csv("result_흥미롭다.csv", encoding='utf-8-sig')
+all_urls = [first_word_url]
+# 행복하다
+sent_dc = pd.DataFrame(columns=["word",'sentiment',"level"])
+first_word_url = "#/entry/koko/b5ae4f305c8942fc9b20537f1bcfa535"
+
+try:
+    search_node(baselink+first_word_url, 1, 0)
+except:
+    pass
+
+print(sent_dc)
+sent_dc.to_csv("result_행복하다.csv", encoding='utf-8-sig')
+all_urls = [first_word_url]
+# 유쾌하다
+sent_dc = pd.DataFrame(columns=["word",'sentiment',"level"])
+first_word_url = "#/entry/koko/f5d7898240de45c5b02ab1a0b0f62998"
+
+try:
+    search_node(baselink+first_word_url, 1, 0)
+except:
+    pass
+
+print(sent_dc)
+sent_dc.to_csv("result_유쾌하다.csv", encoding='utf-8-sig')
+all_urls = [first_word_url]
+# 성공하다
+sent_dc = pd.DataFrame(columns=["word",'sentiment',"level"])
+first_word_url = "#/entry/koko/c4919042e58849cb905680e02d9c7274"
+
+try:
+    search_node(baselink+first_word_url, 1, 0)
+except:
+    pass
+
+print(sent_dc)
+sent_dc.to_csv("result_성공하다.csv", encoding='utf-8-sig')
+all_urls = [first_word_url]
+# 잘하다
+sent_dc = pd.DataFrame(columns=["word",'sentiment',"level"])
+first_word_url = "#/entry/koko/2ab293623c9d443ab8d7a5e38d2df41d"
+
+try:
+    search_node(baselink+first_word_url, 1, 0)
+except:
+    pass
+
+print(sent_dc)
+sent_dc.to_csv("result_잘하다.csv", encoding='utf-8-sig')
+all_urls = [first_word_url]
+# 훌륭하다
+sent_dc = pd.DataFrame(columns=["word",'sentiment',"level"])
+first_word_url = "#/entry/koko/57b2a8ad9671484c835ade92aa79e2f8"
+
+try:
+    search_node(baselink+first_word_url, 1, 0)
+except:
+    pass
+
+print(sent_dc)
+sent_dc.to_csv("result_훌륭하다.csv", encoding='utf-8-sig')
+all_urls = [first_word_url]
+# 좋다
+sent_dc = pd.DataFrame(columns=["word",'sentiment',"level"])
+first_word_url = "#/entry/koko/6cbf564655854b4d8225477320c63e7a"
+
+try:
+    search_node(baselink+first_word_url, 1, 0)
+except:
+    pass
+
+print(sent_dc)
+sent_dc.to_csv("result_좋다.csv", encoding='utf-8-sig')
+all_urls = [first_word_url]
+# 재미없다
+sent_dc = pd.DataFrame(columns=["word",'sentiment',"level"])
+first_word_url = "#/entry/koko/c0f56a6360ef492092dc16b9bb6937e5"
+
+try:
+    search_node(baselink+first_word_url, -1, 0)
+except:
+    pass
+
+print(sent_dc)
+sent_dc.to_csv("result_재미없다.csv", encoding='utf-8-sig')
+all_urls = [first_word_url]'''
+# 화나다
+sent_dc = pd.DataFrame(columns=["word",'sentiment',"level"])
+first_word_url = "#/entry/koko/4a0a2f1d4e514fe98051ec9a275738d7"
+
+try:
+    search_node(baselink+first_word_url, -1, 0)
+except:
+    pass
+
+all_urls = [first_word_url]
+print(sent_dc)
+sent_dc.to_csv("result_화나다.csv", encoding='utf-8-sig')
+
+# 언짢다
+sent_dc = pd.DataFrame(columns=["word",'sentiment',"level"])
+first_word_url = "#/entry/koko/68e26f5324214fb6a829616d70f1a261"
+
+try:
+    search_node(baselink+first_word_url, -1, 0)
+except:
+    pass
+
+print(sent_dc)
+sent_dc.to_csv("result_언짢다.csv", encoding='utf-8-sig')
+all_urls = [first_word_url]
+# 형편없다
+sent_dc = pd.DataFrame(columns=["word",'sentiment',"level"])
+first_word_url = "#/entry/koko/806b30d3bb0442a091f51de3607c7686"
+
+try:
+    search_node(baselink+first_word_url, -1, 0)
+except:
+    pass
+
+print(sent_dc)
+sent_dc.to_csv("result_형편없다.csv", encoding='utf-8-sig')
+all_urls = []
+# 괴롭다
+sent_dc = pd.DataFrame(columns=["word",'sentiment',"level"])
+first_word_url = "#/entry/koko/37cd5ce39cbd4f97a07ed6d6e4000ebf"
+
+try:
+    search_node(baselink+first_word_url, -1, 0)
+except:
+    pass
+
+print(sent_dc)
+sent_dc.to_csv("result_괴롭다.csv", encoding='utf-8-sig')
+all_urls = [first_word_url]
+# 여기서부턴 단어
+# 사랑
+sent_dc = pd.DataFrame(columns=["word",'sentiment',"level"])
+first_word_url = "#/entry/koko/102e6fab94954bb1a61c70b70dc4d781"
+
+try:
+    search_node(baselink+first_word_url, 1, 0)
+except:
+    pass
+
+print(sent_dc)
+sent_dc.to_csv("result_사랑.csv", encoding='utf-8-sig')
+all_urls = [first_word_url]
+# 최악
+sent_dc = pd.DataFrame(columns=["word",'sentiment',"level"])
+first_word_url = "#/entry/koko/ec82b9ddab10453cb74a7802b245bbb5"
+
+try:
+    search_node(baselink+first_word_url, -1, 0)
+except:
+    pass
+
+print(sent_dc)
+sent_dc.to_csv("result_최악.csv", encoding='utf-8-sig')
+all_urls = [first_word_url]
+# 혐오
+sent_dc = pd.DataFrame(columns=["word",'sentiment',"level"])
+first_word_url = "#/entry/koko/e51820bdd27a4733ab00a0a221f5b44a"
+
+try:
+    search_node(baselink+first_word_url, -1, 0)
+except:
+    pass
+
+print(sent_dc)
+sent_dc.to_csv("result_혐오.csv", encoding='utf-8-sig')
+all_urls = [first_word_url]
+# 구닥다리
+sent_dc = pd.DataFrame(columns=["word",'sentiment',"level"])
+first_word_url = "#/entry/koko/f1da4cfceed64a64a1ecd24034f88401"
+
+try:
+    search_node(baselink+first_word_url, -1, 0)
+except:
+    pass
+
+print(sent_dc)
+sent_dc.to_csv("result_구닥다리.csv", encoding='utf-8-sig')
 driver.close()
