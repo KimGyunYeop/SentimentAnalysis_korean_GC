72b246e0efdbdc677133ccbd547bdcfa9dc0a2f7
HyeonJiChoi <44078732+HyeonJiChoi@users.noreply.github.com>
2020-08-26 10:09:40 +0900

    Add Mix att

--- pretrain_embedding/word2vec.py
+++ pretrain_embedding/word2vec.py
@@ -1,33 +1,39 @@
 from konlpy.tag import Okt
 import glob
 import re
-import pandas as pd
-
+from gensim.models import Word2Vec
 file_list = glob.glob("../corpus/*.txt")
 print(file_list)
-
+tokenize_sentences = []
 okt = Okt()
 for file_name in file_list:
     print("file name : ",file_name)
     my_file = open(file_name, "r", encoding="utf16")
     sentences = my_file.readlines()
-    for sentence in sentences:
-        #print('s', sentence)
-        m1 = re.sub(r'<[a-zA-Z.0-9"= ]*>', ' ', sentence)
-        m2 = re.sub(r'</[a-zA-Z.0-9"= ]*>', ' ', m1)
-        m3 = re.sub(r'<[a-zA-Z.0-9"= ]*/>', ' ', m2)
-        if 'vocal desc' in m3:
-            m4 = re.sub('<vocal desc=', ' ', m3)
-            m4 = re.sub('/>', ' ', m4)
-        elif 'event desc' in m3:
-            m4 = re.sub('<event desc=', ' ', m3)
-            idx = m4.index(">")
-            m4 = m4[:idx]
+    start_index = sentences.index("<text>\n")
+    for sentence in sentences[start_index:]:
+        m = re.sub(r'<[a-zA-Z.0-9"= ]*>', '', sentence)
+        m = re.sub(r'</[a-zA-Z.0-9"= ]*>', '', m)
+        m = re.sub(r'<[a-zA-Z.0-9"= ]*/>', '', m)
+        m_ = re.sub(r'<[a-zA-Z0-9."= ]*"[^0-9]*"/>', '', m)
+
+
+        if 'vocal desc' in m_:
+            m = re.sub('<vocal desc=', '', m_)
+            m = re.sub('/>', ' ', m)
+        elif 'event desc' in m_:
+            m = re.sub('<event desc=', '', m_)
+            idx = m.index(">")
+            m = m[:idx]
         else:
-            m4 = m3
+            m = m_
 
-        print(m4)
-
-        found_sentence = ""
-
-        print(found_sentence)
+        found_sentence = re.sub(r'[\t\n\r]', '', m_)
+        print(m)
+        if len(found_sentence.replace(" ", "")) > 0:
+            tokenize_sentence = list(okt.morphs(found_sentence))
+            tokenize_sentences.append(tokenize_sentence)
+embedding_model = Word2Vec.load('ko.bin')
+embedding_model.train(tokenize_sentences, total_examples=len(tokenize_sentences), epochs=10)
+print(len(embedding_model.wv.vocab))
+embedding_model.save("word2vec.model")
