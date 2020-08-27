a0e167ec08092069ebbb1a46af41c33f8dc0c7ac
HyeonJiChoi <44078732+HyeonJiChoi@users.noreply.github.com>
2020-08-27 18:26:44 +0900

    Add Refining embedding

--- pretrain_embedding/word2vec_bigCorpus.py
+++ pretrain_embedding/word2vec_bigCorpus.py
@@ -10,7 +10,7 @@ okt = Okt()
 for file_name in file_list:
     print("file name : ",file_name)
     u'\xc5'.encode('utf-8')
-    my_file = open(file_name, "r", encoding="EUCKR")
+    my_file = open(file_name, "r", encoding="utf8")
     sentences = my_file.readlines()
     for sentence in sentences:
         print(sentence)
