721edb67e75c918109f3d2c429ae8293a189a709
HyeonJiChoi <44078732+HyeonJiChoi@users.noreply.github.com>
2020-08-28 14:24:01 +0900

    Add Refining embedding

--- pretrain_embedding/refining_embeddings.py
+++ pretrain_embedding/refining_embeddings.py
@@ -91,7 +91,7 @@ model.train()
 previous_weight=[]
 for epoch in range(100):
     optimizer.zero_grad()
-    previous_weight.append(model.linear.weight.values().t())
+    previous_weight.append(model.linear.weight.data)
     if epoch>=1:
         loss = model(previous_weight[epoch-1],tmp_data, neighbors)
     else:
