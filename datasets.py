7010d970b38fe0e3c197589aabf131b3f6e0035f
HyeonJiChoi <44078732+HyeonJiChoi@users.noreply.github.com>
2020-09-01 21:05:34 +0900

    Add Refining embedding

--- datasets.py
+++ datasets.py
@@ -94,6 +94,7 @@ class GensimDataset(Dataset):
         for i, token in enumerate(tokens):
             try:
                 print(token)
+                print(self.pretrain_emb.wv[token])
                 data[i] = self.pretrain_emb.wv[token]
                 print(self.pretrain_emb.wv[token])
                 print(data[i])
