57b5bb9a3f73ada753b6ca8ee2b6848d1c76c684
HyeonJiChoi <44078732+HyeonJiChoi@users.noreply.github.com>
2020-09-03 15:37:49 +0900

    Add Refining embedding

--- datasets.py
+++ datasets.py
@@ -82,8 +82,7 @@ class GensimDataset(Dataset):
         self.dataset = pd.read_csv(data_path, encoding="utf8", sep="\t")
         if "small" in mode:
             self.dataset = self.dataset[:10000]
-        self.vocab = Word2Vec.load("pretrain_embedding/word2vec_refining.model").vocabulary
-        print(self.vocab)
+        self.vocab = Word2Vec.load("pretrain_embedding/word2vec_refining.model").wv.vocabulary
 
     def __len__(self):
         return len(self.dataset)
