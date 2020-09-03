4b49f54780c00d085f1015c1e5dfa6b5dca99c70
HyeonJiChoi <44078732+HyeonJiChoi@users.noreply.github.com>
2020-09-03 14:48:51 +0900

    Add Refining embedding

--- datasets.py
+++ datasets.py
@@ -91,7 +91,7 @@ class GensimDataset(Dataset):
         txt = str(self.dataset.at[idx,"review"])
         tokens = self.tokenizer.morphs(txt)
         data = np.zeros((self.maxlen,200)).tolist()
-        data = self.pretrain_emb(tokens)
+        data = self.pretrain_emb.wv.word_vec(tokens)
         for i, token in enumerate(tokens):
             try:
                 data[i] = self.pretrain_emb.wv[token]
