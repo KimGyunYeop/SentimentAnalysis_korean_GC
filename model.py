3b4c1a2c248fc500a39063b3172aa9384e2d9090
HyeonJiChoi <44078732+HyeonJiChoi@users.noreply.github.com>
2020-09-05 00:22:17 +0900

    Fix EMB ATT MODEL

--- model.py
+++ model.py
@@ -1746,13 +1746,13 @@ class EMB_ATT_LSTM_ATT_ver2(nn.Module):
         return attn_output
 
     def sentiment_net(self, lstm_outputs):
-        result = self.word_dense(lstm_outputs)
+        result = self.word_dense(torch.tanh(lstm_outputs))
         sig_output = self.softmax(result)
         batch_size, max_len, _=sig_output.shape
         zeros = torch.zeros(batch_size, max_len, dtype=torch.long).to(self.config.device)
         ones = torch.ones(batch_size, max_len, dtype=torch.long).to(self.config.device)
         emb_result = self.sentiment_embedding(zeros) * sig_output[:,:,0].unsqueeze(-1).repeat(1,1,768) + self.sentiment_embedding(ones) * sig_output[:,:,1].unsqueeze(-1).repeat(1,1,768)
-        senti_output = lstm_outputs * emb_result
+        senti_output = torch.tanh(lstm_outputs * emb_result)
         return senti_output
 
     def forward(self, input_ids, attention_mask, labels, token_type_ids):
