201b121247c88f755a09f100693f857857092f4f
KimGyunYeop <gyop0817@naver.com>
2020-09-09 10:10:10 +0900

    fix_error

--- getMaxAcc.py
+++ getMaxAcc.py
@@ -2,6 +2,7 @@ import pandas as pd
 import os
 import argparse
 import numpy as np
+import torch
 
 args = argparse.ArgumentParser()
 
@@ -12,6 +13,11 @@ args = args.parse_args()
 if "ckpt/" in args.result_dir:
     args.result_dir = args.result_dir[5:]
 
+setting_path = os.path.join("ckpt", args.result_dir, "checkpoint-best")
+setting = torch.load(os.path.join("training_args_1.bin"))
+for i in setting:
+    print(i)
+
 result_path = os.path.join("ckpt", args.result_dir, "test")
 epoch_list = os.listdir(result_path)
 
@@ -20,6 +26,7 @@ for i in epoch_list:
     with open(os.path.join(result_path,i),"r") as fp:
         acc_dict[int(i[5:-4])] = float(fp.readline().split()[-1])
 
+reversed_dict = { x:y for x,y in acc_dict.items()}
 acc_dict = sorted(acc_dict.items())
 
 print('epoch\tacc')
@@ -28,3 +35,4 @@ for (file, acc) in acc_dict:
 
 acc_dict = list(map(list,sorted(acc_dict)))
 print("\nmax = ",max(np.array(acc_dict)[:,-1]))
+print("max step = ",reversed_dict[max(np.array(acc_dict)[:,-1])])
