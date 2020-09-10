fddebf8a0238ec98aa785dc45758c4c0d7c96d30
HyeonJiChoi <44078732+HyeonJiChoi@users.noreply.github.com>
2020-09-11 00:57:50 +0900

    Fix Optimizer

--- train.py
+++ train.py
@@ -54,8 +54,8 @@ def train(args,
     optimizer_grouped_parameters = [
         {'params': [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)],
          'weight_decay': args.weight_decay},
-        {'params': [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay) and any(nd in n for nd in weight_decay_change)], 'weight_decay': 0.0},
-        {'params': [p for n, p in model.named_parameters() if not any(nd in n for nd in weight_decay_change)], 'weight_decay': 0.3}
+        {'params': [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay) and not (nd in n for nd in weight_decay_change)], 'weight_decay': 0.0},
+        {'params': [p for n, p in model.named_parameters() if nd in n for nd in weight_decay_change], 'weight_decay': 0.3}
     ]
     optimizer = AdamW(optimizer_grouped_parameters, lr=args.learning_rate, eps=args.adam_epsilon)
     scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=args.warmup_steps,
