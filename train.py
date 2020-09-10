5d8e42180ac0640e5af6b0368156abe9dee902d9
HyeonJiChoi <44078732+HyeonJiChoi@users.noreply.github.com>
2020-09-10 22:53:31 +0900

    Fix Optimizer

--- train.py
+++ train.py
@@ -12,6 +12,7 @@ from torch import nn
 from torch.utils.data import DataLoader, RandomSampler, SequentialSampler
 from transformers import (
     AdamW,
+    Adafactor,
     get_linear_schedule_with_warmup
 )
 #test
@@ -55,7 +56,7 @@ def train(args,
          'weight_decay': args.weight_decay},
         {'params': [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
     ]
-    optimizer = AdamW(optimizer_grouped_parameters, lr=args.learning_rate, eps=args.adam_epsilon)
+    optimizer = Adafactor(optimizer_grouped_parameters, lr=args.learning_rate, eps=args.adam_epsilon)
     scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=args.warmup_steps,
                                                 num_training_steps=t_total)
 
