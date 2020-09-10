403c8f94b00a0e989ecb58b853b09de11dffe9a3
HyeonJiChoi <44078732+HyeonJiChoi@users.noreply.github.com>
2020-09-10 23:09:48 +0900

    Fix Optimizer

--- model.py
+++ model.py
@@ -2379,8 +2379,6 @@ class EMB_ATT_LSTM_ATT_ver2(nn.Module):
         self.word_dense = nn.Linear(768, 2)
         self.sentiment_embedding = nn.Embedding(2, 768)
         self.softmax = nn.Softmax(dim=-1)
-        self.batchnormal = nn.BatchNorm2d(768)
-
         # attention module
         self.att_w = nn.Parameter(torch.randn(1, 768, 1))
 
@@ -2406,7 +2404,7 @@ class EMB_ATT_LSTM_ATT_ver2(nn.Module):
         zeros = torch.zeros(batch_size, max_len, dtype=torch.long).to(self.config.device)
         ones = torch.ones(batch_size, max_len, dtype=torch.long).to(self.config.device)
         emb_result = self.sentiment_embedding(zeros) * sig_output[:,:,0].unsqueeze(-1).repeat(1,1,768) + self.sentiment_embedding(ones) * sig_output[:,:,1].unsqueeze(-1).repeat(1,1,768)
-        senti_output = self.gelu(self.batchnormal(lstm_outputs + emb_result))
+        senti_output = self.gelu(lstm_outputs + emb_result)
         return senti_output
 
     def forward(self, input_ids, attention_mask, labels, token_type_ids):
