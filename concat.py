629454200eb9e93bb802106af0309a4591fae272
lyn <parksoyoung1205@daum.net>
2020-09-11 13:49:17 +0900

    add aniplus_Reviews + youtube negative + youtube positive

--- concat.py
+++ concat.py
@@ -5,6 +5,9 @@ f4_f = open('../crawling/4flix_Reviews.txt', encoding='utf-8-sig')
 kino_f = open('../crawling/kinolights_Reviews.txt', encoding='utf-8-sig')
 ip_f = open('../crawling/sports_interpark_Reviews.txt', encoding='utf-8-sig')
 watcha_f = open('../crawling/watcha_Reviews.txt', encoding='utf-8-sig')
+ani_f =  open('../crawling/RA/aniplus_Reviews.txt', encoding='utf-8-sig')
+yn_f = open('../crawling/RA/youtube_comments_negative.txt', encoding='utf-8-sig')
+yp_f = open('../crawling/RA/youtube_comments_positive.txt', encoding='utf-8-sig')
 
 list_all = base_f.readlines()
 print('len all', len(list_all))
@@ -105,8 +108,62 @@ for i in range(len(list_ip)):
     arr_ip.append(str(list_ip[i][0])+'\t'+str(list_ip[i][1])+'\t'+str(list_ip[i][2])+'\n')
 print('interpark length', len(arr_ip))
 
-# append nsmc, 4flix,kino, watcha, interpark list
-list_all = list_all + arr_f4 + arr_kino + arr_wc + arr_ip
+# remove duplications from 'RA/aniplus_Reviews.txt' and convert to string list
+ani_df = pd.DataFrame(columns = ['review_id', 'review', 'rating'])
+lines = ani_f.readlines()
+for i in range(1, len(lines)):
+    sp = lines[i].split('\t')
+    a = {"review_id" : start + idx_idx, "review" : sp[1], "rating" : int(sp[2])}
+    idx_idx += 1
+    ani_df = ani_df.append(a, ignore_index=True)
+print(len(ani_df))
+ani_df = ani_df.drop_duplicates(subset = ['review'])
+print(len(ani_df))
+# list to array
+list_ani = ani_df.values.tolist()
+arr_ani = []
+for i in range(len(list_ani)):
+    arr_ani.append(str(list_ani[i][0])+'\t'+str(list_ani[i][1])+'\t'+str(list_ani[i][2])+'\n')
+print('aniplus length', len(arr_ani))
+
+# remove duplications from 'RA/youtube_comments_negative.txt' and convert to string list
+yn_df = pd.DataFrame(columns = ['review_id', 'review', 'rating'])
+lines = yn_f.readlines()
+for i in range(1, len(lines)):
+    sp = lines[i].split('\t')
+    a = {"review_id" : start + idx_idx, "review" : sp[1], "rating" : 0}
+    idx_idx += 1
+    yn_df = yn_df.append(a, ignore_index=True)
+print(len(yn_df))
+yn_df = yn_df.drop_duplicates(subset = ['review'])
+print(len(yn_df))
+# list to array
+list_yn = yn_df.values.tolist()
+arr_yn = []
+for i in range(len(list_yn)):
+    arr_yn.append(str(list_yn[i][0])+'\t'+str(list_yn[i][1])+'\t'+str(list_yn[i][2])+'\n')
+print('youtube negative length', len(list_yn))
+
+# remove duplications from 'RA/youtube_comments_positive.txt' and convert to string list
+yp_df = pd.DataFrame(columns = ['review_id', 'review', 'rating'])
+lines = yp_f.readlines()
+for i in range(1, len(lines)):
+    sp = lines[i].split('\t')
+    a = {"review_id" : start + idx_idx, "review" : sp[1], "rating" : 1}
+    idx_idx += 1
+    yp_df = yp_df.append(a, ignore_index=True)
+print(len(yp_df))
+yp_df = yp_df.drop_duplicates(subset = ['review'])
+print(len(yp_df))
+# list to array
+list_yp = yp_df.values.tolist()
+arr_yp = []
+for i in range(len(list_yp)):
+    arr_yp.append(str(list_yp[i][0])+'\t'+str(list_yp[i][1])+'\t'+str(list_yp[i][2])+'\n')
+print('youtube positive length', len(list_yp))
+
+# append nsmc, 4flix,kino, watcha, interpark, aniplus list
+list_all = list_all + arr_f4 + arr_kino + arr_wc + arr_ip + arr_ani + arr_yn +  arr_yp
 print(len(list_all))
 f = open("all_Reviews.txt", 'w', encoding='utf-8-sig')
 for i in range(len(list_all)):
@@ -118,3 +175,6 @@ f4_f.close()
 kino_f.close()
 ip_f.close()
 watcha_f.close()
+ani_f.close()
+yn_f.close()
+yp_f.close()
