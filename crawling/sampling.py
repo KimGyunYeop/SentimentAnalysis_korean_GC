5ca347750328d37ce033684b38ea03ad788e398b
lyn <parksoyoung1205@daum.net>
2020-09-12 04:24:18 +0900

    fix sampling

--- crawling/sampling.py
+++ crawling/sampling.py
@@ -2,9 +2,9 @@ import pandas as pd
 df = pd.read_csv('daumMovie_Reviews.txt', sep='\t')
 df = df.drop_duplicates(subset = ['reviews'])
 pos = df[df['label']==1]
-print(len(pos))
+print('daum pos', len(pos))
 neg = df[df['label']==0]
-print(len(neg))
+print('daum neg', len(neg))
 
 pos_sample = pos.sample(n=10000)
 print(len(pos_sample))
@@ -14,29 +14,32 @@ print(len(neg_sample))
 pos_sample.to_csv('daum_posSample.txt')
 neg_sample.to_csv('daum_negSample.txt')
 
-df = pd.read_csv('naverMovie_Reviews_2016.txt', sep='\t')
-df = df.drop_duplicates(subset = ['reviews'])
+all_df = pd.read_csv('naverMovie_Reviews_2016.txt', sep='\t')
+all_df = all_df.drop_duplicates(subset = ['reviews'])
 pos = df[df['label']==1]
 print(len(pos))
 neg = df[df['label']==0]
 print(len(neg))
 df = pd.read_csv('naverMovie_Reviews_2017.txt', sep='\t')
 df = df.drop_duplicates(subset = ['reviews'])
+all_df = all_df.append(df)
 pos = df[df['label']==1]
 print(len(pos))
 neg = df[df['label']==0]
 print(len(neg))
 df = pd.read_csv('naverMovie_Reviews_2018.txt', sep='\t')
 df = df.drop_duplicates(subset = ['reviews'])
+all_df = all_df.append(df)
 pos = df[df['label']==1]
 print(len(pos))
 neg = df[df['label']==0]
 print(len(neg))
 df = pd.read_csv('naverMovie_Reviews_2019.txt', sep='\t')
 df = df.drop_duplicates(subset = ['reviews'])
-pos = df[df['label']==1]
+all_df = all_df.append(df)
+pos = all_df[all_df['label']==1]
 print(len(pos))
-neg = df[df['label']==0]
+neg = all_df[all_df['label']==0]
 print(len(neg))
 
 pos_sample = pos.sample(n=10000)
