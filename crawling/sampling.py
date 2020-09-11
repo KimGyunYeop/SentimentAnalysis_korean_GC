913292cf072da016d86cc9efe31c7bb757ba7056
lyn <parksoyoung1205@daum.net>
2020-09-12 04:04:59 +0900

    drop duplicates

--- crawling/sampling.py
+++ crawling/sampling.py
@@ -1,9 +1,6 @@
 import pandas as pd
 df = pd.read_csv('daumMovie_Reviews.txt', sep='\t')
-print(len(df))
-ip_df = df.drop_duplicates(subset = ['label'])
-print(len(df))
-#print(df[:5])
+df = df.drop_duplicates(subset = ['reviews'])
 pos = df[df['label']==1]
 print(len(pos))
 neg = df[df['label']==0]
@@ -17,40 +14,30 @@ print(len(neg_sample))
 pos_sample.to_csv('daum_posSample.txt')
 neg_sample.to_csv('daum_negSample.txt')
 
-
 df = pd.read_csv('naverMovie_Reviews_2016.txt', sep='\t')
-print(len(df))
-ip_df = df.drop_duplicates(subset = ['label'])
-print(len(df))
-#print(df[:5])
+df = df.drop_duplicates(subset = ['reviews'])
 pos = df[df['label']==1]
 print(len(pos))
 neg = df[df['label']==0]
 print(len(neg))
 df = pd.read_csv('naverMovie_Reviews_2017.txt', sep='\t')
-print(len(df))
-ip_df = df.drop_duplicates(subset = ['label'])
-print(len(df))
-pos = pos.append(df[df['label']==1])
-print('pos', len(pos))
-neg = neg.append(df[df['label']==0])
-print('neg', len(neg))
+df = df.drop_duplicates(subset = ['reviews'])
+pos = df[df['label']==1]
+print(len(pos))
+neg = df[df['label']==0]
+print(len(neg))
 df = pd.read_csv('naverMovie_Reviews_2018.txt', sep='\t')
-print(len(df))
-ip_df = df.drop_duplicates(subset = ['label'])
-print(len(df))
-pos = pos.append(df[df['label']==1])
-print('pos', len(pos))
-neg = neg.append(df[df['label']==0])
-print('neg', len(neg))
+df = df.drop_duplicates(subset = ['reviews'])
+pos = df[df['label']==1]
+print(len(pos))
+neg = df[df['label']==0]
+print(len(neg))
 df = pd.read_csv('naverMovie_Reviews_2019.txt', sep='\t')
-print(len(df))
-ip_df = df.drop_duplicates(subset = ['label'])
-print(len(df))
-pos = pos.append(df[df['label']==1])
-print('pos', len(pos))
-neg = neg.append(df[df['label']==0])
-print('neg', len(neg))
+df = df.drop_duplicates(subset = ['reviews'])
+pos = df[df['label']==1]
+print(len(pos))
+neg = df[df['label']==0]
+print(len(neg))
 
 pos_sample = pos.sample(n=10000)
 print(len(pos_sample))
