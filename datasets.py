3f2a3580e4bd66f58ba17c18b15dd520754e79c4
HyeonJiChoi <44078732+HyeonJiChoi@users.noreply.github.com>
2020-09-13 22:17:37 +0900

    Add augmentation datasets.py

--- datasets.py
+++ datasets.py
@@ -96,7 +96,6 @@ class AugmentBaseDataset(Dataset):
         for word in lexicon_words:
             print(random.choice(self.lexicon_dic[word]))
             txt = txt.replace(word, random.choice(self.lexicon_dic[word]))
-        if "기쁘" in txt:
             print(txt)
         data = self.tokenizer(txt, pad_to_max_length=True, max_length=self.maxlen, truncation=True)
         input_ids = torch.LongTensor(data["input_ids"])
