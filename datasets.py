7ba0690dd327fdcea8840012b658ce8697a91e4a
HyeonJiChoi <44078732+HyeonJiChoi@users.noreply.github.com>
2020-09-13 22:08:28 +0900

    Add augmentation datasets.py

--- datasets.py
+++ datasets.py
@@ -60,7 +60,7 @@ class AugmentBaseDataset(Dataset):
 
         self.lexicon_dic = self.get_lexicon2dic(self.lexicon)
 
-        self.re_compile_words = re.compile(r"\b(" + "|".join(self.lexicon_dic.keys()) + ")\b")
+        self.re_compile_words = re.compile(r"(" + "|".join(self.lexicon_dic.keys()) + ")")
 
     def get_lexicon2dic(self,lexicon):
         lexicon_dic = {}
