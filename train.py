a0d8fed8e9bc8ff41a8ed27643182e3c453f9bcf
HyeonJiChoi <44078732+HyeonJiChoi@users.noreply.github.com>
2020-09-13 22:20:49 +0900

    Add augmentation datasets.py

--- train.py
+++ train.py
@@ -56,7 +56,7 @@ def train(args,
         {'params': [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay) and not any(n in a for a in weight_decay_change)], 'weight_decay': 0.0},
         {'params': [p for n, p in model.named_parameters() if any(n in a for a in weight_decay_change)], 'weight_decay': 0.3}
     ]
-    print([p for n, p in model.named_parameters() if any(n in a for a in weight_decay_change)])
+    print([n for n, p in model.named_parameters() if any(n in a for a in weight_decay_change)])
     optimizer = AdamW(optimizer_grouped_parameters, lr=args.learning_rate, eps=args.adam_epsilon)
     scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=args.warmup_steps,
                                                 num_training_steps=t_total)
