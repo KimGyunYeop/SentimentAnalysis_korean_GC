16ecfb40a6c15d802fcfa2c2e91e2103876b9e23
HyeonJiChoi <44078732+HyeonJiChoi@users.noreply.github.com>
2020-09-14 21:06:17 +0900

    Fix VoSenti

--- datasets.py
+++ datasets.py
@@ -62,7 +62,7 @@ class AugmentBaseDataset(Dataset):
 
         self.lexicon_dic = self.get_lexicon2dic(self.lexicon)
 
-        self.re_compile_words = re.compile(r"\b(" + "|".join(self.lexicon_dic.keys()) + ")")
+        self.re_compile_words = re.compile(r"\b(" + "|".join(self.lexicon_dic.keys()) + ")\\W", re.I)
 
     def get_lexicon2dic(self,lexicon):
         lexicon_dic = {}
