2915513f8c24da48a07697a748980bc96f2f39ab
lyn <parksoyoung1205@daum.net>
2020-09-18 13:34:52 +0900

    naver movie use all negative data

--- concat.py
+++ concat.py
@@ -1,4 +1,3 @@
-'''
 # concat sampled naver, daum movie review + tv(4flix, watcha, kinolights, aniplus), interpark, youtube, samsung lions
 import pandas as pd
 n_df_neg = pd.read_csv('./crawling/naver_negSample.txt', sep=',')
@@ -9,16 +8,7 @@ n_df_neg = n_df_neg.drop(columns=['Unnamed: 0', 'Unnamed: 0.1'])
 n_df = n_df_neg.append(n_df_pos)
 print(len(n_df_neg), len(n_df_pos), len(n_df))
 n_df = n_df.rename(columns={'label':'rating', 'reviews' : 'review'})
-#n_df = n_df.drop_duplicates(subset = ['review'])
-d_df_neg = pd.read_csv('./crawling/daum_negSample.txt', sep=',')
-d_df_pos = pd.read_csv('./crawling/daum_posSample.txt', sep=',')
-d_df_pos = d_df_pos.drop(columns=['Unnamed: 0', 'Unnamed: 0.1'])
-d_df_neg = d_df_neg.drop(columns=['Unnamed: 0', 'Unnamed: 0.1'])
-d_df = d_df_neg.append(n_df_pos)
-print(len(d_df_neg), len(d_df_pos), len(d_df))
-d_df = d_df.rename(columns={'label':'rating', 'reviews' : 'review'})
-#d_df = d_df.drop_duplicates(subset = ['review'])
-all_df = n_df.append(d_df)
+
 all_df = n_df
 print(len(all_df))
 f4_df = pd.read_csv('./crawling/4flix_Reviews.txt', sep='\t')
@@ -121,7 +111,7 @@ list = all_df.values.tolist()
 arr = []
 idx = 0
 ip_f = open("./crawling/all_ip.txt", encoding='utf-8-sig')
-ori_f = open("./data/nsmc/ratings_train.txt", encoding='utf-8-sig')
+ori_f = open("data/nsmc/ratings_train.txt", encoding='utf-8-sig')
 arr = ori_f.readlines()
 arr_ip = ip_f.readlines()
 for i in range(len(list)):
@@ -130,15 +120,8 @@ for i in range(len(list)):
 print('ip', len(arr_ip))
 arr = arr + arr_ip
 print(len(arr))
-f = open("./data/nsmc/final.txt", 'w', encoding='utf-8-sig')
+f = open("./data/final_train.txt", 'w', encoding='utf-8-sig')
 f.writelines(arr)
 f.close()
-'''
-import pandas as pd
-ori_f = pd.read_csv('./data/nsmc/ratings_train.txt', sep='\t')
-#ori_f = open("./data/nsmc/ratings_train.txt", encoding='utf-8-sig')
-#print(ori_f)
 
-print(ori_f.columns)
-print(len(ori_f[ori_f['rating']==0]))
-print(len(ori_f[ori_f['rating']==1]))
+
--- crawling/sampling.py
+++ crawling/sampling.py
@@ -1,4 +1,5 @@
 import pandas as pd
+'''
 df = pd.read_csv('daumMovie_Reviews.txt', sep='\t')
 df = df.drop_duplicates(subset = ['reviews'])
 pos = df[df['label']==1]
@@ -10,16 +11,19 @@ pos_sample = pos.sample(n=20000)
 print(len(pos_sample))
 neg_sample = neg.sample(n=20000)
 print(len(neg_sample))
-
 pos_sample.to_csv('daum_posSample.txt')
 neg_sample.to_csv('daum_negSample.txt')
-
+'''
+l_neg=0
+l_pos=0
 all_df = pd.read_csv('naverMovie_Reviews_2016.txt', sep='\t')
 all_df = all_df.drop_duplicates(subset = ['reviews'])
-pos = df[df['label']==1]
+pos = all_df[all_df['label']==1]
 print(len(pos))
-neg = df[df['label']==0]
+neg = all_df[all_df['label']==0]
 print(len(neg))
+l_pos+=len(pos)
+l_neg+=len(neg)
 df = pd.read_csv('naverMovie_Reviews_2017.txt', sep='\t')
 df = df.drop_duplicates(subset = ['reviews'])
 all_df = all_df.append(df)
@@ -27,6 +31,9 @@ pos = df[df['label']==1]
 print(len(pos))
 neg = df[df['label']==0]
 print(len(neg))
+l_pos+=len(pos)
+l_neg+=len(neg)
+
 df = pd.read_csv('naverMovie_Reviews_2018.txt', sep='\t')
 df = df.drop_duplicates(subset = ['reviews'])
 all_df = all_df.append(df)
@@ -34,6 +41,9 @@ pos = df[df['label']==1]
 print(len(pos))
 neg = df[df['label']==0]
 print(len(neg))
+l_pos+=len(pos)
+l_neg+=len(neg)
+
 df = pd.read_csv('naverMovie_Reviews_2019.txt', sep='\t')
 df = df.drop_duplicates(subset = ['reviews'])
 all_df = all_df.append(df)
@@ -41,10 +51,13 @@ pos = all_df[all_df['label']==1]
 print(len(pos))
 neg = all_df[all_df['label']==0]
 print(len(neg))
+print('pos: ',l_pos, 'neg: ', l_neg)
+l_pos+=len(pos)
+l_neg+=len(neg)
 
-pos_sample = pos.sample(n=20000)
+pos_sample = pos.sample(n=12423)
 print(len(pos_sample))
-neg_sample = neg.sample(n=25000)
+neg_sample = neg.sample(n=12423)
 print(len(neg_sample))
 
 pos_sample.to_csv('naver_posSample.txt')
